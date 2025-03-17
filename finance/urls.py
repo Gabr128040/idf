d43c82e6b2a42c7f73cf48bece251eb69ca57a3f
Gabr128040 <gabr.128040@gmail.com>
2025-03-17 19:33:35 -0300

    register e login

--- finance/urls.py
+++ finance/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import TransacaoListCreateView, TransacaoRetrieveUpdateDestroyView
+from .views import user_register, user_login
+
 
 urlpatterns = [
-   path('transacoes/', TransacaoListCreateView.as_view(), name='transacao-list-create'),
-   path('transacoes/<int:pk>/', TransacaoRetrieveUpdateDestroyView.as_view(), name='transacao-detail'),
+  path('transacoes/', TransacaoListCreateView.as_view(), name='transacao-list-create'),
+  path('transacoes/<int:pk>/', TransacaoRetrieveUpdateDestroyView.as_view(), name='transacao-detail'),
+  path('register/', user_register, name='register'),
+  path('login/', user_login, name='login'),
 ]
--- finance/views.py
+++ finance/views.py
@@ -13,3 +13,40 @@ class TransacaoRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
    queryset = Transacao.objects.all()
    serializer_class = TransacaoSerializer
 # Create your views here.
+
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+
+@api_view(['POST'])
+def user_register(request):
+   username = request.data.get('username')
+   password = request.data.get('password')
+   email = request.data.get('email')
+
+   if User.objects.filter(username=username).exists():
+       return Response({'error': 'Usuário já existe'}, status=status.HTTP_400_BAD_REQUEST)
+
+   user = User.objects.create_user(username=username, password=password, email=email)
+   refresh = RefreshToken.for_user(user)
+   return Response({
+       'refresh': str(refresh),
+       'access': str(refresh.access_token),
+   }, status=status.HTTP_201_CREATED)
+
+@api_view(['POST'])
+def user_login(request):
+   username = request.data.get('username')
+   password = request.data.get('password')
+   user = User.objects.filter(username=username).first()
+
+   if user and user.check_password(password):
+       refresh = RefreshToken.for_user(user)
+       return Response({
+           'refresh': str(refresh),
+           'access': str(refresh.access_token),
+       }, status=status.HTTP_200_OK)
+
+   return Response({'error': 'Credenciais inválidas'}, status=status.HTTP_400_BAD_REQUEST)
